cdf4750948d6b3f09d86dc121a69901660bd7623
Binaya <sbinayaraj@gmail.com>
2025-09-06 13:11:21 +0300

    header added and css for header added

--- src/app/componnets/Header.tsx
+++ src/app/componnets/Header.tsx
@@ -1,37 +1,19 @@
 "use client"
-import { Menu, X } from 'lucide-react';
 import Link from 'next/link';
 import React, { useState } from 'react';
 const Header = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
   return (
     <header className="header">
-      <nav className={`navbar ${isMobileMenuOpen ? 'navbar-active' : ''}`}>
+      <nav className={`navbar`}>
         <ul className="navbar-list">
           <li>
             <Link className="navbar-link home-link" href="/">Home</Link>
           </li>
           <li>
-            <Link className="navbar-link service-link" href="#service-section">Skills</Link>
-          </li>
-          <li>
-            <Link className="navbar-link portfolio-link" href="#portfolio-section">Project</Link>
-          </li>
-          <li>
-            <Link className="navbar-link contact-link" href="#section-contact">Contact</Link>
+            <Link className="navbar-link service-link" href="/codes">Codes</Link>
           </li>
         </ul>
       </nav>
-      <div className="mobile-navbar-btn" onClick={toggleMobileMenu}>
-        {isMobileMenuOpen ? (
-          <X className="mobile-nav-icon" />
-        ) : (
-          <Menu className="mobile-nav-icon" />
-        )}
-      </div>
     </header>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import ContactSection from "./componnets/ContactSection";
 import HeroSection from "./componnets/HeroSection";
 import PortfolioSection from "./componnets/PortfolioSection";
 import Footer from "./componnets/Footer";
+import Header from "./componnets/Header";
 export default function Home() {
   return (
     <>
+    <Header/>
       <HeroSection />
       <PortfolioSection />
       <ContactSection />
