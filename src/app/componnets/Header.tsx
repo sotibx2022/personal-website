a29ad67bbc18a4f4b0223685c6244eb57b6b1850
Binaya <sbinayaraj@gmail.com>
2025-09-06 13:36:12 +0300

    active state added to the link

--- src/app/componnets/Header.tsx
+++ src/app/componnets/Header.tsx
@@ -1,27 +1,37 @@
 "use client"
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { Home, Code2 } from 'lucide-react';
+import './Header.css';
 const Header = () => {
+  const [activeLink, setActiveLink] = useState('/');
   return (
-      <header className="header">
-        <nav className="navbar">
-          <ul className="navbar-list">
-            <li>
-              <Link className="navbar-link home-link" href="/">
-                <Home size={20} />
-                <span>Home</span>
-              </Link>
-            </li>
-            <li>
-              <Link className="navbar-link service-link" href="/codes">
-                <Code2 size={20} />
-                <span>Codes</span>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-  )
-}
+    <header className="header">
+      <nav className="navbar">
+        <ul className="navbar-list">
+          <li>
+            <Link 
+              className={`navbar-link ${activeLink === '/' ? 'active-link' : ''}`} 
+              href="/"
+              onClick={() => setActiveLink('/')}
+            >
+              <Home size={20} />
+              <span>Home</span>
+            </Link>
+          </li>
+          <li>
+            <Link 
+              className={`navbar-link ${activeLink === '/codes' ? 'active-link' : ''}`} 
+              href="/codes"
+              onClick={() => setActiveLink('/codes')}
+            >
+              <Code2 size={20} />
+              <span>Codes</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 export default Header;
